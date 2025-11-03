f6c4ffc096b103164ac56fe65265fef3191f7368
Abi One <loremsey@tuta.me>
2025-11-03 15:41:30 +0300

    Shaderc version update

--- vulkano-shaders/src/codegen.rs
+++ vulkano-shaders/src/codegen.rs
@@ -145,9 +145,8 @@ pub(super) fn compile(
     shader_kind: ShaderKind,
 ) -> Result<(CompilationArtifact, Vec<String>), String> {
     let includes = RefCell::new(Vec::new());
-    let compiler = Compiler::new().ok_or("failed to create GLSL compiler")?;
-    let mut compile_options =
-        CompileOptions::new().ok_or("failed to initialize compile options")?;
+    let compiler = Compiler::new().expect("failed to create GLSL compiler");
+    let mut compile_options = CompileOptions::new().expect("failed to initialize compile options");
 
     compile_options.set_target_env(
         TargetEnv::Vulkan,
